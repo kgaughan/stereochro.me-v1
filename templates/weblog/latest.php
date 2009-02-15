f996c87e9362a7163e688d2e28558a996e77ced6
Keith Gaughan <keith@talideon.com>
2009-02-15 19:37:23 +0000

    Latest links page was missing the add link.

--- templates/weblog/latest.php
+++ templates/weblog/latest.php
@@ -1,3 +1,6 @@
 <?php $this->with_envelope() ?>
+<?php start_slot('page-navigation') ?>
+	<li><a href="/weblog/;add">Add</a></li>
+<?php end_slot() ?>
 <?php start_slot('title') ?>Latest links<?php end_slot() ?>
 <?php $this->render_each('entry-body', $entries, 'no-entries') ?>
