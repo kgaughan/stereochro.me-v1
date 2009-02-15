66abc93f4fc5070bcb2600e345532841fdc94967
Keith Gaughan <keith@talideon.com>
2009-02-15 13:41:45 +0000

    Pulled the logic for saving a page out to its own method.

--- handlers/PageHandler.php
+++ handlers/PageHandler.php
@@ -9,9 +9,9 @@ class PageHandler extends AFK_HandlerBase {
 		}
 
 		$page = $db->query_row('
-			SELECT	title, content
-			FROM	pages
-			WHERE	slug = %s', $ctx->slug);
+			SELECT title, content
+			FROM   pages
+			WHERE  slug = %s', $ctx->slug);
 		if (empty($page)) {
 			$page = array('title' => '', 'content' => '');
 			if ($ctx->view() == 'edit' && AFK_Users::current()->is_logged_in()) {
@@ -24,30 +24,33 @@ class PageHandler extends AFK_HandlerBase {
 		$ctx->merge($page);
 	}
 
-	public function on_put_view(AFK_Context $ctx) {
+	private function save($slug, $title, $content, $user_id) {
 		global $db;
+		if ($db->query_value('SELECT COUNT(*) FROM pages WHERE slug = %s', $slug) == 0) {
+			$now = time();
+			$db->insert('pages', array(
+				'slug' => $slug,
+				'title' => $title,
+				'content' => $content,
+				'time_c' => $now,
+				'time_m' => $now,
+				'user_id_c' => $user_id,
+				'user_id_m' => $user_id));
+		} else {
+			$db->execute('
+				UPDATE pages
+				SET    title = %s, content = %s, time_m = UNIX_TIMESTAMP(NOW()), user_id_m = %d
+				WHERE  slug = %s
+				', $title, $content, $user_id, $slug);
+		}
+	}
 
+	public function on_put_view(AFK_Context $ctx) {
 		AFK_Users::prerequisites('edit');
 		if (isset($ctx->preview)) {
 			$ctx->change_view('preview');
 		} else {
-			if ($db->query_value('SELECT COUNT(*) FROM pages WHERE slug = %s', $ctx->slug) == 0) {
-				$now = time();
-				$db->insert('pages', array(
-					'slug' => $ctx->slug,
-					'title' => $ctx->title,
-					'content' => $ctx->content,
-					'time_c' => $now,
-					'time_m' => $now,
-					'user_id_c' => AFK_Users::current()->get_id(),
-					'user_id_m' => AFK_Users::current()->get_id()));
-			} else {
-				$db->execute('
-					UPDATE	pages
-					SET		title = %s, content = %s, time_m = UNIX_TIMESTAMP(NOW()), user_id_m = %d
-					WHERE	slug = %s
-					', $ctx->title, $ctx->content, AFK_Users::current()->get_id(), $ctx->slug);
-			}
+			$this->save($ctx->slug, $ctx->title, $ctx->content, AFK_Users::current()->get_id());
 			$ctx->allow_rendering(false);
 			$ctx->redirect();
 		}
