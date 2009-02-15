1fed24379fd455e795f380062bcce935c5c199be
Keith Gaughan <keith@talideon.com>
2009-02-15 00:02:55 +0000

    Added code to put something in the title slot.

--- templates/root/edit.php
+++ templates/root/edit.php
@@ -1,4 +1,11 @@
 <?php $this->with_envelope() ?>
+<?php start_slot('title') ?>
+	<?php if (trim($title) == '') { ?>
+		New page
+	<?php } else { ?>
+		Editing &lsquo;<?php ee($title) ?>&rsquo;
+	<?php } ?>
+<?php end_slot() ?>
 <form method="post" action="<?php ee($ctx->application_root(), $slug) ?>">
 <h1><input type="text" name="title" id="title" value="<?php ee($title) ?>"></h1>
 <p><textarea name="content" rows="25"><?php ee($content) ?></textarea></p>
--- templates/root/view.php
+++ templates/root/view.php
@@ -1,3 +1,4 @@
 <?php $this->with_envelope() ?>
+<?php start_slot('title') ?><?php ee($title) ?><?php end_slot() ?>
 <h1><?php ee($title) ?></h1>
 <?php echo Markdown($content) ?>
