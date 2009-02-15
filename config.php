f0ab4ebf58d5ac63a92d0cbc1ac060c0fa549106
Keith Gaughan <keith@talideon.com>
2009-02-15 13:23:13 +0000

    Trimmed junk from the configuration and make markdown a helper.

--- config.php
+++ config.php
@@ -5,11 +5,6 @@ if (file_exists(APP_ROOT . '/site-config.php')) {
 	require(APP_ROOT . '/deployment/configurations/default.php');
 }
 
-require(APP_ROOT . '/lib/markdown.php');
-
-define('APP_VERSION', '0.0.0');
-define('WITH_LOGGING', STATUS == 'LIVE' || STATUS == 'STAGING');
-
 function routes() {
 	$r = new AFK_Routes(array(
 		'slug' => '[-a-z0-1.]+(?:/[-a-z0-1.]+)?|',
@@ -35,17 +30,9 @@ function init() {
 
 	error_reporting(E_ALL);
 	date_default_timezone_set('UTC');
-	AFK::load_helper('core', 'html', 'slots');
+	AFK::load_helper('core', 'html', 'slots', 'markdown');
 
 	$db = new DB_MySQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-	// If you need sessions, you might need this:
-	// new AFK_Session_DB($db, 'sessions');
-	// session_start();
-
-	// If you need output caching, you'll need this:
-	// AFK::load_helper('cache');
-	// cache_install(new AFK_Cache_DB($db, 'cache'));
 
 	AFK_Users::set_implementation(new Users('talideon.com'));
 
