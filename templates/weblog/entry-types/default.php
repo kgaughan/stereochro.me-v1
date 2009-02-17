1559d7690a35c5e6672059f17d6c1cc463888554
Keith Gaughan <keith@talideon.com>
2009-02-17 03:07:32 +0000

    Tweaked the spacing of the weblog entries so that they're just right.

--- templates/weblog/entry-types/default.php
+++ templates/weblog/entry-types/default.php
@@ -6,7 +6,9 @@
 		<?php } ?>
 	</div>
 
+	<?php if ($title != '') { ?>
 	<h2><a href="<?php ee($link) ?>"><?php echo format_line($title) ?></a></h2>
+	<?php } ?>
 	<?php if ($note != '') { ?>
 		<?php echo format($note) ?>
 	<?php } ?>
