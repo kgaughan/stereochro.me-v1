4dbbe5defb37e66813b465efede46477cc7e3a1e
Keith Gaughan <keith@talideon.com>
2009-02-17 02:46:03 +0000

    Vaguely bearable via links.

--- templates/weblog/entry-types/default.php
+++ templates/weblog/entry-types/default.php
@@ -1,9 +1,12 @@
 <div class="entry">
 	<div class="meta">
 		<a href="/weblog/<?php echo $id ?>"><?php ee(date(WEBLOG_DATE, $time_c)) ?></a>
+		<?php if ($via != '') { ?>
+		<br>[<a href="<?php ee($via) ?>">source</a>]
+		<?php } ?>
 	</div>
 
-	<p><a href="<?php ee($link) ?>"><?php echo format_line($title) ?></a></p>
+	<h2><a href="<?php ee($link) ?>"><?php echo format_line($title) ?></a></h2>
 	<?php if ($note != '') { ?>
 		<?php echo format($note) ?>
 	<?php } ?>
--- templates/weblog/latest.php
+++ templates/weblog/latest.php
@@ -1,3 +1,3 @@
 <?php $this->with_envelope('weblog') ?>
-<?php start_slot('title') ?>Latest links<?php end_slot() ?>
+<?php start_slot('title') ?>Latest inklings<?php end_slot() ?>
 <?php $this->render_each('entry-body', $entries, 'no-entries') ?>
