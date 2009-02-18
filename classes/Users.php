34c9be6bdc13cb71f41329994adefda53525fed9
Keith Gaughan <keith@talideon.com>
2009-02-18 02:47:20 +0000

    As AFK_HttpAuthUsers does all this stuff anyway, derive Users from it instead.

--- classes/Users.php
+++ classes/Users.php
@@ -1,55 +1,24 @@
 <?php
-class Users extends AFK_Users {
+class Users extends AFK_HttpAuthUsers {
 
-	private $realm;
-	private $id;
-
-	public function __construct($realm) {
-		$this->realm = $realm;
-		$this->id = null;
+	protected function authenticate($username, $hash) {
+		global $db;
+		return $db->query_value(
+			"SELECT id FROM users WHERE uname = %s AND pwd = %s",
+			$username, $hash);
 	}
 
-	protected function get_current_user_id() {
-		global $db;
-
-		$ctx = AFK_Registry::context();
-		if (is_null($this->id) && $ctx->__isset('PHP_AUTH_USER')) {
-			$this->id = $db->query_value("
-				SELECT	id
-				FROM	users
-				WHERE	uname = %1\$s AND
-						pwd = MD5(CONCAT(%1\$s, ':', %3\$s, ':', %2\$s))
-				", $ctx->PHP_AUTH_USER, $ctx->PHP_AUTH_PW, $this->realm);
-		}
-
-		if (is_null($this->id)) {
-			return 0;
-		}
-
-		return $this->id;
+	protected function get_anonymous_user() {
+		return new AFK_User(AFK_Users::ANONYMOUS, 'An Anonymous Hero');
 	}
 
 	protected function load(array $ids) {
 		global $db;
-		if (!$this->has(0)) {
-			$anon = new AFK_User(0, 'An Anonymous Hero');
-			$this->add($anon);
-		}
 		$db->query("SELECT id, uname FROM users WHERE id IN (%d)", $ids);
 		while ($r = $db->fetch()) {
 			$user = new AFK_User($r['id'], $r['uname']);
 			$user->add_capabilities(array('edit'));
 			$this->add($user);
-		}
-	}
-
-	protected function require_auth() {
-		static $called = false;
-		if (!$called) {
-			$called = true;
-			throw new AFK_HttpException(
-				'You are not authorised for access.', 401,
-				array('WWW-Authenticate' => "Basic realm=\"{$this->realm}\""));
 		}
 	}
 }
