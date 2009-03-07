124edd637e3dce78dc80888aba16bd4aa59dd07e
Keith Gaughan <keith@talideon.com>
2009-03-07 13:19:32 +0000

    Link to feed.

--- templates/weblog/weblog.envelope.php
+++ templates/weblog/weblog.envelope.php
@@ -2,6 +2,9 @@
 $this->with_envelope();
 $this->render_each('archive-summary', WeblogData::get_archive_summary());
 ?>
+<?php start_slot('head') ?>
+	<link title="Feed" rel="alternate" href="/weblog/;feed" type="application/atom+xml">
+<?php end_slot_append() ?>
 <?php start_slot('page-navigation') ?>
 	<li><a href="/weblog/;add">Add</a></li>
 <?php end_slot_append() ?>
