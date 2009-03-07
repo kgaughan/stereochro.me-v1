4d16886d0d0ad4d1393f34d3884ff1bf6745a65e
Keith Gaughan <keith@talideon.com>
2009-03-07 13:19:08 +0000

    Prevent indexing of archives.

--- templates/weblog/month.php
+++ templates/weblog/month.php
@@ -2,6 +2,9 @@
 $this->with_envelope('weblog');
 $this->render_each('entry-body', $entries, 'no-entries');
 ?>
+<?php start_slot('head') ?>
+	<meta name="robots" content="noindex, follow">
+<?php end_slot_append() ?>
 <?php start_slot('title') ?>
 	Inklings in <?php ee(date('F Y', $ts)) ?>
 <?php end_slot() ?>
