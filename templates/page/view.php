8cc0e11104088e25b407fe0a10342fcd0ca7346c
Keith Gaughan <keith@talideon.com>
2009-03-07 20:47:33 +0000

    Mushed together, this was hard to follow - spread it out.

--- templates/page/view.php
+++ templates/page/view.php
@@ -4,11 +4,13 @@
 
 <?php if (cache('page:' . $ctx->REQUEST_URI)) { ?>
 	<h1><?php get_slot('title') ?></h1>
+
 	<?php echo format($content) ?>
+
 	<div id="metadata">
-	Created at <?php ee(date(PAGE_DATE, $time_c)) ?>
-	<?php if ($time_c != $time_m) { ?>
-	; last modified at <?php ee(date(PAGE_DATE, $time_m)) ?>
-	<?php } ?>
+		Created at <?php ee(date(PAGE_DATE, $time_c)) ?>
+		<?php if ($time_c != $time_m) { ?>
+		; last modified at <?php ee(date(PAGE_DATE, $time_m)) ?>
+		<?php } ?>
 	</div>
 <?php cache_end(); } ?>
