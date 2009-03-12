f074a835dc2de79482b70dd1ff16d5a1f7fc75d2
Keith Gaughan <keith@talideon.com>
2009-03-12 02:43:38 +0000

    Automagical embedding of YouTube videos.

--- templates/weblog/entry-body.php
+++ templates/weblog/entry-body.php
@@ -1,10 +1,19 @@
 <div class="entry">
-	<div class="meta"><a href="/weblog/<?php echo $id ?>">&#182;</a></div>
+	<div class="meta"><a href="/weblog/<?php echo $id ?>">&infin;</a></div>
 
 	<?php if ($title != '' && $link != '') { ?>
 		<h2><a href="<?php ee($link) ?>"><?php echo format_line($title) ?></a></h2>
 	<?php } elseif ($title != '') { ?>
 		<h2><?php echo format_line($title) ?></h2>
+	<?php } ?>
+
+	<?php if (preg_match('~^http://www.youtube.com/watch\?v=([A-Za-z0-9_]{11,})~', $link, $matches)) { ?>
+		<?php $movie_url = "http://www.youtube.com/v/{$matches[1]}&rel=1" ?>
+		<div class="illustration">
+		<object width="425" height="355" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
+		<param name="movie" value="<?php ee($movie_url) ?>">
+		</object>
+		</div>
 	<?php } ?>
 
 	<?php if ($note != '') { ?>
