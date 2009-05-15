3c903e4cd2d34f2e51ec251b59a274dbc919c7d0
Keith Gaughan <keith@talideon.com>
2009-05-15 19:45:41 +0100

    DailyMotion autoembed support.

--- templates/weblog/entry-body.php
+++ templates/weblog/entry-body.php
@@ -14,6 +14,9 @@
 		<div class="illustration">
 		<object width="425" height="355" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
 		<param name="movie" value="<?php ee($movie_url) ?>">
+		<param name="FlashVars" value="quality=best&amp;scale=showAll">
+		<param name="quality" value="best">
+		<param name="scale" value="showAll">
 		</object>
 		</div>
 	<?php } elseif (preg_match('~^http://video\.google\.com/videoplay\?docid=(-?\d+)~', $link, $matches)) { ?>
@@ -21,13 +24,25 @@
 		<div class="illustration">
 		<object width="400" height="326" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
 		<param name="movie" value="<?php ee($movie_url) ?>">
-		<param name="FlashVars" value="playerMode=embedded">
+		<param name="FlashVars" value="playerMode=embedded&amp;quality=best&amp;scale=showAll">
+		<param name="quality" value="best">
+		<param name="scale" value="showAll">
 		</object>
 		</div>
 	<?php } elseif (preg_match('~^http://(?:www\.)?vimeo\.com/(\d+)~', $link, $matches)) { ?>
 		<?php $movie_url = "http://www.vimeo.com/moogaloop.swf?clip_id=" . $matches[1] ?>
 		<div class="illustration">
 		<object width="500" height="300" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
+		<param name="movie" value="<?php ee($movie_url) ?>">
+		<param name="FlashVars" value="quality=best&amp;scale=showAll">
+		<param name="quality" value="best">
+		<param name="scale" value="showAll">
+		</object>
+		</div>
+	<?php } elseif (preg_match('~^http://(?:www\.)?dailymotion\.com/video/([a-z0-9]+)~', $link, $matches)) { ?>
+		<?php $movie_url = "http://www.dailymotion.com/swf/" . $matches[1] ?>
+		<div class="illustration">
+		<object width="420" height="339" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
 		<param name="movie" value="<?php ee($movie_url) ?>">
 		<param name="FlashVars" value="quality=best&amp;scale=showAll">
 		<param name="quality" value="best">
