3a0057a842c3d89654bbf3bdc6720afdba085911
Keith Gaughan <keith@talideon.com>
2009-10-25 18:30:32 +0000

    Extracted the code for generating flash embeds to a pair of functions.

--- lib/lib.php
+++ lib/lib.php
@@ -17,3 +17,45 @@ function is_naked_day($d) {
 	$now = time() + $z;
 	return $now >= $start && $now <= $end;
 }
+
+function generate_flash_embed($movie_url, $width, $height, array $params) {
+	$params_html = '<param name="movie" value="' . e($movie_url) . '">';
+	if (count($params) > 0) {
+		$params_html .= '<params name="FlashVars" value="' . e(http_build_query($params)) . '">';
+		foreach ($params as $name => $value) {
+			$params_html .= sprintf('<param name="%s" value="%s">', e($name), e($value));
+		}
+	}
+	return sprintf(
+		'<object type="application/x-shockwave-flash" data="%s" width="%s" height="%s">%s</object>',
+		e($movie_url), $width, $height, $params_html);
+}
+
+function generate_link_embed($link) {
+	static $patterns = array(
+		'http://(?:www\.)?youtube\.com/watch\?v=([-A-Za-z0-9_]{11,})' => array(
+			'pattern' => "http://www.youtube.com/v/%s&rel=1",
+			'width' => 425,
+			'height' => 355),
+		'http://video\.google\.com/videoplay\?docid=(-?\d+)' => array(
+			'pattern' => "http://video.google.com/googleplayer.swf?hl=en&docId=%s",
+			'width' => 400,
+			'height' => 326),
+		'http://(?:www\.)?vimeo\.com/(\d+)' => array(
+			'pattern' => "http://www.vimeo.com/moogaloop.swf?clip_id=%s",
+			'width' => 500,
+			'height' => 300),
+		'http://(?:www\.)?dailymotion\.com/video/([a-z0-9]+)' => array(
+			'pattern' => "http://www.dailymotion.com/swf/%s",
+			'width' => 420,
+			'height' => 339));
+
+	foreach ($patterns as $pattern => $params) {
+		if (preg_match("~^$pattern~", $link, $matches)) {
+			return generate_flash_embed(
+				sprintf($params['pattern'], $matches[1]),
+				$params['width'], $params['height'], array());
+		}
+	}
+	return false;
+}
--- templates/weblog/entry-body.php
+++ templates/weblog/entry-body.php
@@ -14,46 +14,8 @@
 		<h2><?php echo format_line($title) ?></h2>
 	<?php } ?>
 
-	<?php if (preg_match('~^http://(?:www\.)?youtube\.com/watch\?v=([-A-Za-z0-9_]{11,})~', $link, $matches)) { ?>
-		<?php $movie_url = "http://www.youtube.com/v/{$matches[1]}&rel=1" ?>
-		<div class="illustration">
-		<object width="425" height="355" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
-		<param name="movie" value="<?php ee($movie_url) ?>">
-		<param name="FlashVars" value="quality=best&amp;scale=showAll">
-		<param name="quality" value="best">
-		<param name="scale" value="showAll">
-		</object>
-		</div>
-	<?php } elseif (preg_match('~^http://video\.google\.com/videoplay\?docid=(-?\d+)~', $link, $matches)) { ?>
-		<?php $movie_url = "http://video.google.com/googleplayer.swf?hl=en&docId=" . $matches[1] ?>
-		<div class="illustration">
-		<object width="400" height="326" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
-		<param name="movie" value="<?php ee($movie_url) ?>">
-		<param name="FlashVars" value="playerMode=embedded&amp;quality=best&amp;scale=showAll">
-		<param name="quality" value="best">
-		<param name="scale" value="showAll">
-		</object>
-		</div>
-	<?php } elseif (preg_match('~^http://(?:www\.)?vimeo\.com/(\d+)~', $link, $matches)) { ?>
-		<?php $movie_url = "http://www.vimeo.com/moogaloop.swf?clip_id=" . $matches[1] ?>
-		<div class="illustration">
-		<object width="500" height="300" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
-		<param name="movie" value="<?php ee($movie_url) ?>">
-		<param name="FlashVars" value="quality=best&amp;scale=showAll">
-		<param name="quality" value="best">
-		<param name="scale" value="showAll">
-		</object>
-		</div>
-	<?php } elseif (preg_match('~^http://(?:www\.)?dailymotion\.com/video/([a-z0-9]+)~', $link, $matches)) { ?>
-		<?php $movie_url = "http://www.dailymotion.com/swf/" . $matches[1] ?>
-		<div class="illustration">
-		<object width="420" height="339" type="application/x-shockwave-flash" data="<?php ee($movie_url) ?>">
-		<param name="movie" value="<?php ee($movie_url) ?>">
-		<param name="FlashVars" value="quality=best&amp;scale=showAll">
-		<param name="quality" value="best">
-		<param name="scale" value="showAll">
-		</object>
-		</div>
+	<?php if (($embed = generate_link_embed($link)) !== false) { ?>
+		<div class="illustration"><?php echo $embed ?></div>
 	<?php } ?>
 
 	<?php if ($note != '') { ?>
