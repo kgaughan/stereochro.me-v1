e86b2a9956c298c2993f46249cb3c962583ac8c8
Keith Gaughan <keith@talideon.com>
2010-01-24 20:33:09 +0000

    Remove Google urchin markup.

--- templates/default.envelope.php
+++ templates/default.envelope.php
@@ -58,15 +58,7 @@
 			</div>
 		</div>
 
+		<?php javascript(array('prettify')) ?><script type="text/javascript">prettyPrint();</script>
 		<?php get_slot('post_body') ?>
-		<?php javascript(array('prettify')) ?>
-		<script type="text/javascript">
-		var gaJsHost = "https:" == document.location.protocol ? "https://ssl." : "http://www.";
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		</script>
-		<script type="text/javascript">
-		try { var pageTracker = _gat._getTracker("UA-2914483-4"); pageTracker._trackPageview(); } catch(err) {}
-		prettyPrint();
-		</script>
 	</body>
 </html>
