a071629d5cca6d52c13b691ebe13422780923e1c
Keith Gaughan <keith@talideon.com>
2010-01-24 20:37:36 +0000

    Added Google Analytics urchin plugin.

---
+++ plugins/urchin/plugin.php
@@ -0,0 +1,23 @@
+<?php
+class UrchinPlugin extends Plugin {
+
+	protected function get_events() {
+		return array('slot:post_body' => 'embed_urchin');
+	}
+
+	public function embed_urchin($msg, $v) {
+		$tracker = $this->get_setting('tracker');
+		if (!empty($tracker)) {
+			$ctx = AFK_Registry::_('ctx');
+
+			$src = ($ctx->is_secure() ? 'https://ssl' : 'http://www') . '.google-analytics.com/ga.js';
+			printf('<script src="%s" type="text/javascript"></script>', e($src));
+
+			printf('<script type="text/javascript">try{_gat._getTracker("%s")._trackPageview();}catch(err){}</script>',
+				e($tracker));
+		}
+		return array(false, false);
+	}
+}
+
+new UrchinPlugin();
